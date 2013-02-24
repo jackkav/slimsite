e8c75af364910f712fe0b94f310bc86507223656
Jack <jackkav@gmail.com>
2013-02-24 20:40:41 +0000

    Added email form

--- Gemfile
+++ Gemfile
@@ -5,6 +5,8 @@ gem "sass"
 gem "dm-core"
 gem "dm-migrations"
 gem "thin"
+gem "sinatra-flash"
+gem "pony"
 gem "pg", :group => :production
 gem "dm-postgres-adapter", :group => :production
 gem "dm-sqlite-adapter", :group => :development
--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'slim'
 require 'sass'
+require 'pony'
+require 'sinatra/flash'
 require './song.rb'
 
 configure :development do
@@ -12,11 +14,39 @@ configure :production do
 end
 configure do
 	enable :sessions
+	set :username, 'frank'
+	set :password, 'sinatra'
+	set :session_secret, 'try to make this long and hard to guess'
 end
 configure do
 	set :port, 1337
 end
+
 get('/styles.css'){ scss :styles }
+
+helpers do
+	def css(*stylesheets)
+	stylesheets.map do |stylesheet|
+		"<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
+		end.join
+	end
+
+	def current?(path='/')
+		(request.path==path || request.path==path+'/') ? "current" : nil
+	end
+end
+
+before do
+	set_title
+end
+
+def set_title
+	@title ||= "Songs By Sinatra"
+end
+
+get '/set/:name' do
+	session[:name] = params[:name]
+end
 
 get '/login' do
 	slim :login
@@ -46,6 +76,38 @@ end
 get '/contact' do
 	slim :contact
 end
+
+post '/contact' do
+	send_message
+	flash[:notice] = "Thank you for your message. We'll be in touch soon."
+	redirect to('/')
+end
+
+def send_message
+	Pony.mail(
+		:from => params[:name] + "<" + params[:email] + ">",
+		:to => 'jackkav@gmail.com',
+		:subject => params[:name] + " has contacted you",
+		:body => params[:message],
+		:port => '587',
+		:via => :smtp,
+		:via_options => {
+			:address
+			=> 'smtp.gmail.com',
+			:port
+			=> '587',
+			:enable_starttls_auto => true,
+			:user_name
+			=> 'daz',
+			:password
+			=> 'secret',
+			:authentication
+			=> :plain,
+			:domain
+			=> 'localhost.localdomain'
+		})
+end
+
 not_found do
 	slim :not_found
 end
