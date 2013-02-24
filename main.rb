eeaec65b1f32904300524b5fd78530c4aff51571
Jack <jackkav@gmail.com>
2013-02-24 17:17:44 +0000

    added database config

--- main.rb
+++ main.rb
@@ -4,6 +4,12 @@ require 'slim'
 require 'sass'
 require './song.rb'
 
+configure :development do
+	DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+end
+configure :production do
+	DataMapper.setup(:default, ENV['DATABASE_URL'])
+end
 configure do
 	set :port, 1337
 end
